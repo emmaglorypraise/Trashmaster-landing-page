676c98d189b648939b567c4b4fcfa2d35f77b00d
Glory Praise <44278865+emmaglorypraise@users.noreply.github.com>
2020-07-17 12:06:07 +0100

    Update process.php

--- process.php
+++ process.php
@@ -8,7 +8,7 @@ if (isset($_POST['emailsaver'])){
 
     $email=$_POST['email'];
 
-    $mysqli =new mysqli('127.0.0.1','root','','trashmaster') or die(mysqli_error($mysqli));
+    $mysqli =new mysqli('3306','qcr0wyvurxjts25z','jze41od3su7udcac','eazxk938o1nnui2m') or die(mysqli_error($mysqli));
     
 
     $_SESSION["success_message"] = "you have subscribed sucessfully";
